607f6aad45b29ae968f266380610afaf4bbfa85c
Richard Amering <richard.amering@gmx.at>
2020-11-21 21:05:37 +0100

    resetting tracking

--- Code/main.m
+++ Code/main.m
@@ -1,11 +1,12 @@
 % Model Simulation: Group 3
 % Main File
-sum([1; 2; 3])
+
+x = readtable("../Data/Peng_Robinson.txt");
+T = x{:,1}';
+P = x{:,1}';
+
 nn = Network([1,4,5,1],"sigmoid","cross-entropy");
-%nn.train([0.5, 0.5, 0.6], [1,2,3], 1, 0.01)
-%nn.train(3, 4, 1, 0.01)
+nn.train(T, P, 32, 0.01)
 
-[dC_dW_backprob, dC_db_backprob, dCdW_linear, dCdB_linear] = nn.gradient_checker(3,4);
-
-dC_db_backprob'
-dCdB_linear
+[P_nn] = nn.forward(T)
+nn.train(3, 4, 1, 0.01)
