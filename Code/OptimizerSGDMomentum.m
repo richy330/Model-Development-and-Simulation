3641a4ce22660c64b81ede68020ec3271cffe03e
Richard Amering <richard.amering@gmx.at>
2020-12-19 17:33:10 +0100

    simplified SGDMomentum

--- Code/OptimizerSGDMomentum.m
+++ Code/OptimizerSGDMomentum.m
@@ -1,58 +1,38 @@
-classdef OptimizerSGDMomentum < IOptimizer & handle
+classdef OptimizerSGDMomentum < IOptimizer & handle & matlab.mixin.Copyable
     
     properties
         beta
-        layer_mom_map
+        dCdW_mom
+        dCdb_mom
+        hyperparams = struct()
     end
     
     methods
         %% Constructor
         function obj = OptimizerSGDMomentum(beta)
             obj.beta = beta;
-            obj.layer_mom_map = containers.Map('KeyType', 'double', 'ValueType','any');
+            obj.dCdW_mom = 0;
+            obj.dCdb_mom = 0;
+            obj.hyperparams.beta = beta;
         end
         
         %% Descend
         function descend(obj, parent_layer, eta_m, lambda)
             [dCdW, dCdb] = obj.get_gradient(parent_layer);
-            [dCdW_mom, dCdb_mom] = obj.get_momentum(parent_layer);
             
-            W_corr = obj.beta*dCdW_mom + (1-obj.beta)*eta_m * (dCdW + lambda*parent_layer.W);
-            b_corr = obj.beta*dCdb_mom + (1-obj.beta)*eta_m * dCdb;
+            W_corr = obj.beta*obj.dCdW_mom + (1-obj.beta)*eta_m * (dCdW + lambda*parent_layer.W);
+            b_corr = obj.beta*obj.dCdb_mom + (1-obj.beta)*eta_m * dCdb;
             parent_layer.W = parent_layer.W - W_corr;
             parent_layer.b = parent_layer.b - b_corr;
             
-            obj.store_momentum(parent_layer, W_corr, b_corr);
+            obj.dCdW_mom = W_corr;
+            obj.dCdb_mom = b_corr;
         end
-        
         
         %% Get gradient
         function [dCdW, dCdb] = get_gradient(obj, parent_layer)
             dCdW = [parent_layer.delta * parent_layer.prev.a']';
             dCdb = sum(parent_layer.delta, 2);
-        end
-        
-        %% Get Momentum
-        function [dCdW, dCdb] = get_momentum(obj, parent_layer)
-            map = obj.layer_mom_map;
-            uuid = parent_layer.uuid;
-            
-            if ~isKey(map, uuid)
-                momentum = struct;
-                momentum.W_corr = zeros(size(parent_layer.W));
-                momentum.b_corr = zeros(size(parent_layer.b));
-                map(uuid) = momentum;
-                obj.layer_mom_map = map;
-            end
-            dCdW = obj.layer_mom_map(uuid).W_corr;
-            dCdb = obj.layer_mom_map(uuid).b_corr;
-        end
-        
-        %% Store Momentum
-        function store_momentum(obj, parent_layer, W_corr, b_corr)
-            momentum = struct('W_corr', W_corr, 'b_corr', b_corr);
-            uuid = parent_layer.uuid;
-            obj.layer_mom_map(uuid) = momentum;
         end
     end
 end
