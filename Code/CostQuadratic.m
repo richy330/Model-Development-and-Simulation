39e69450ea68cbf4a9278435c9b70b66243864a7
Richard Amering <richard.amering@gmx.at>
2020-12-21 15:34:33 +0100

    added Cost-Gradient clipping

--- Code/CostQuadratic.m
+++ Code/CostQuadratic.m
@@ -2,6 +2,7 @@ classdef CostQuadratic < ICost
     
     properties
         hyperparams = struct('Costfunction', 'Quadratic')
+        costder_limit = 1
     end
     
     methods
@@ -11,6 +12,11 @@ classdef CostQuadratic < ICost
         
         function cost_der = derivative(obj, a, y)
             cost_der = a - y;
+            
+            % Gradient Clipping
+            if any(abs(cost_der) > obj.costder_limit)
+                cost_der = cost_der * obj.costder_limit/max(abs(cost_der));
+            end
         end
     end
 end
