a5db0fb601ed046ca3b6e9a97a52cd4aa1421790
Richard Amering <richard.amering@gmx.at>
2020-12-30 22:43:36 +0100

    Gradient clipping limit now scales with size of gradient-matrix

--- Code/get_gradient.m
+++ Code/get_gradient.m
@@ -1,16 +1,20 @@
 function [dCdW, dCdb] = get_gradient(parent_layer)
 % Calculating Gradients from a given layer. Scaling Gradients down if they
 % exceed a certain limit.
-    gradient_limit = 10;
     
     dCdW = [parent_layer.delta * parent_layer.prev.a']';
     dCdb = sum(parent_layer.delta, 2);
     
-    if norm(dCdW) > gradient_limit
-        dCdW = dCdW * gradient_limit/norm(dCdW);
+    % limits clip when norm(gradient) is equal to norm(ones-matrix)
+    dCdW_limit = sqrt(numel(dCdW));
+    dCdb_limit = sqrt(numel(dCdb));
+    
+    
+    if norm(dCdW) > dCdW_limit
+        dCdW = dCdW * dCdW_limit/norm(dCdW);
     end
-    if norm(dCdb) > gradient_limit
-        dCdb = dCdb * gradient_limit/norm(dCdb);
+    if norm(dCdb) > dCdb_limit
+        dCdb = dCdb * dCdb_limit/norm(dCdb);
     end    
 end
 
